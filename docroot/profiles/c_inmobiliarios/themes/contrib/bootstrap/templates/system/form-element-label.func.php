2b0317b13bb9570d58ffb4dfde4c49af506ec7cd
Carlos Cámara <carlos.camara@gmail.com>
2016-09-09 22:22:53 +0200

    Updated boostrap theme to 3.6 version.

--- docroot/profiles/c_inmobiliarios/themes/contrib/bootstrap/templates/system/form-element-label.func.php
+++ docroot/profiles/c_inmobiliarios/themes/contrib/bootstrap/templates/system/form-element-label.func.php
@@ -37,11 +37,14 @@ function bootstrap_form_element_label(&$variables) {
 
   // Extract variables.
   $output = '';
-  $title = isset($element['#title']) ? filter_xss_admin($element['#title']) . ' ' : '';
-  $required = !empty($element['#required']) ? theme('form_required_marker', array('element' => $element)) : '';
-  if ($required) {
-    $title .= $required;
+
+  $title = !empty($element['#title']) ? filter_xss_admin($element['#title']) : '';
+
+  // Only show the required marker if there is an actual title to display.
+  if ($title && $required = !empty($element['#required']) ? theme('form_required_marker', array('element' => $element)) : '') {
+    $title .= ' ' . $required;
   }
+
   $display = isset($element['#title_display']) ? $element['#title_display'] : 'before';
   $type = !empty($element['#type']) ? $element['#type'] : FALSE;
   $checkbox = $type && $type === 'checkbox';
@@ -49,7 +52,7 @@ function bootstrap_form_element_label(&$variables) {
 
   // Immediately return if the element is not a checkbox or radio and there is
   // no label to be rendered.
-  if (!$checkbox && !$radio && ($display === 'none' || (!$title && !$required))) {
+  if (!$checkbox && !$radio && ($display === 'none' || !$title)) {
     return '';
   }
 
